ba263f090d7f0f83e87be8fcb87a9e71e974c629
Adam P. Goucher <goucher@statslab.cam.ac.uk>
2020-10-11 14:58:12 +0100

    Bump submodule

--- src/apgluxe.hpp
+++ src/apgluxe.hpp
@@ -1,22 +1,4 @@
 #pragma once
 
-#include <algorithm>
-
-#include "../apgmera/lifelib/upattern.h"
-#include "../apgmera/lifelib/classifier.h"
-#include "../apgmera/lifelib/incubator.h"
-
-#define APG_VERSION "ikpx2.2-" LIFELIB_VERSION
-
-#include "../apgmera/includes/utilities.h"
-#include "../apgmera/includes/params2.h"
-#include "../apgmera/includes/md5.h"
-#include "../apgmera/includes/payosha256.h"
-#include "../apgmera/includes/hashsoup2.h"
-
-#define LIFETREE_MEM 100
-
-#include "../apgmera/includes/detection.h"
-#include "../apgmera/includes/stabilise.h"
-#include "../apgmera/includes/searcher.h"
-
+#define PREFIX_VERSION "ikpx2.2-"
+#include "../apgmera/includes/base.h"
